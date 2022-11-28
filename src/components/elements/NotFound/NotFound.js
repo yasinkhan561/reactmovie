6ebfb86a09d1db7d4f85e926313e4e7d2be33497
YASIN KHAN <yasinkhan26608@gmail.com>
2022-11-28 20:29:17 +0400

    initial commit

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const NotFound = () => {
+
+	return(
+          <div>
+            <h1> Whoops page doesn't exist</h1>
+          </div>
+		)
+}
+
+
+export default NotFound;
